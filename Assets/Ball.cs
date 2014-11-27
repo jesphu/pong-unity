fcae15ea3cd447e9c37e3d1f3b46ce3fcd83a584
Jessica Phuong <jessicaphuong02@gmail.com>
2014-11-27 17:45:54 +1030

    Submit current version of the pong clone

---
+++ Assets/Ball.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	// speed to be modified in the inspector
+	public float speed = 4.0f;
+
+	void Start() {
+		// give the ball some initial movement direction
+		rigidbody2D.velocity = Vector2.one.normalized * speed;
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+		// 1 (at the top of the racket) 
+		// 0 (at the middle of the racket)
+		// -1 (at the bottom of the racket)
+		return (ballPos.y - racketPos.y) / racketHeight;
+		// we subtract the racketPos.y from the ballPos.y to have a relative position
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		// this function is called whenever the ball
+		// collides with something
+
+		if (col.gameObject.name == "RacketLeft") { // hit the left racket
+			// calculate hit factor
+			float y = hitFactor(transform.position, col.transform.position, ((BoxCollider2D)col.collider).size.y);
+
+			// calculate direction, set length to 1
+			Vector2 dir = new Vector2(1, y).normalized;
+
+			// set veolicty with dir * speed
+			rigidbody2D.velocity = dir * speed;
+		}
+
+		if (col.gameObject.name == "RacketRight") { // hit the right racket
+			float y = hitFactor(transform.position, col.transform.position, ((BoxCollider2D)col.collider).size.y);
+
+			// calculate direction, set length to 1
+			Vector2 dir = new Vector2(-1, y).normalized;
+
+			// set veolicty with dir * speed
+			rigidbody2D.velocity = dir * speed;
+		}
+	}
+}
---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour {
+
+	// up and down keys (to be set in the inspector)
+	public KeyCode up;
+	public KeyCode down;
+	void FixedUpdate() {
+		if (Input.GetKey(up)) { // up key pressed
+			transform.Translate(new Vector2(0.0f, 0.1f)); // in this case, we add nothing to the x-coordinate but we add 0.1 or -0.1
+		}												  // to the y-coordinate to move the racket upwards/downwards a bit
+		if (Input.GetKey (down)){ // down key pressed
+			transform.Translate (new Vector3(0.0f, -0.1f));
+		}
+	}
+}
